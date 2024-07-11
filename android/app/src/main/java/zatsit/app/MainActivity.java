3f8bd5ec3d1b6f8dd1b272ec372e525777913ced
olivier <olivier.goyon@ocus.com>
2024-07-11 18:28:04 +0200

    feat(clean): clean all

--- android/app/src/main/java/zatsit/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package zatsit.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- app.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <h1>Zatsit</h1>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
-</template>
